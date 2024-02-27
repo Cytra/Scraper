0c2a7e3c2b9d3ec47e545a1a1252cc729aa9a2e2
Martynas Samuilovas <martynas.samuilovas@saldo.com>
2024-02-27 23:00:16 +0200

    updated logging

--- README.md
+++ README.md
@@ -4,16 +4,20 @@ docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-chro
 
 you can see what docker is doing on http://localhost:7900 password => secret
 
-# Deploy Selenium to K8s cluster
+# Deploy Selenium to K8s
 
 cd C:\Users\martynas.samuilovas\source\Personal\Scraper\selenium-chart
 helm install selenium-release docker-selenium/selenium-grid -f values.yaml --namespace selenium
 helm install selenium-grid docker-selenium/selenium-grid
-helm uninstall selenium-grid
 
-# Update the deployment
 helm upgrade selenium-release docker-selenium/selenium-grid -f values.yaml --namespace selenium
 
 helm delete selenium-release --namespace selenium
 
-#
+# Deploy Scraper to k8s
+
+cd C:\Users\martynas.samuilovas\source\Personal\Scraper\chart
+kubectl apply -f deployment.yaml
+kubectl delete -f deployment.yaml
+
+kubectl rollout restart deployment scraper-deployment
--- src/Infrastructure/Scrapers/SeleniumDriverFactory.cs
+++ src/Infrastructure/Scrapers/SeleniumDriverFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Options;
 using Application.Ports;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
@@ -9,12 +10,15 @@ namespace Infrastructure.Scrapers;
 public class SeleniumDriverFactory : ISeleniumDriverFactory
 {
     private readonly string _seleniumUrl;
-    public SeleniumDriverFactory(IOptions<AppOptions> options)
+    private readonly ILogger<SeleniumDriverFactory> _logger;
+    public SeleniumDriverFactory(IOptions<AppOptions> options, ILogger<SeleniumDriverFactory> logger)
     {
+        _logger = logger;
         _seleniumUrl = options.Value.SeleniumUrl;
     }
     public RemoteWebDriver GetDriver()
     {
+        _logger.LogInformation("Selenium Url {url}", _seleniumUrl);
         var chromeOptions = new ChromeOptions(); 
         return new RemoteWebDriver(new Uri($"{_seleniumUrl}"), chromeOptions);
     }
